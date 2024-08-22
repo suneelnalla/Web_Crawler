05dc0c40981b92092f5b7b3d4f0ebc549e269152
suneel nalla <136744780+suneelnalla@users.noreply.github.com>
2024-08-22 12:27:26 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -7,8 +7,13 @@ def Request(url):
         
     except requests.exceptions.ConnectionError:
         pass
-target_url = "adulttime.com"
+target_url = "google.com"
+with open("D:\python_scripts\web_crawler\subdomains-wodlist.txt.txt","r") as wordlist_file:
 
+    for line in wordlist_file:
+        word = line.strip()
+        test_url = word + "." + target_url
+        print(test_url)
 with open("D:/python_scripts/web_crawler/files-and-dirs-wordlist.txt","r") as wordlist_file:
     for line in wordlist_file:
         word = line.strip()
@@ -16,3 +21,4 @@ with open("D:/python_scripts/web_crawler/files-and-dirs-wordlist.txt","r") as wo
         response = Request(test_url)
         if response:
             print("[+] Discoverable subdomain -->" + test_url)
+ 
